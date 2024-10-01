468216f156fa4c8b8e92c3eeb6c74483d2d0c3fe
moshudougee <mbuthiadenis@gmail.com>
2024-10-01 04:04:33 +0300

    downgraded native wind to v2 for build purpose
    Signed-off-by: moshudougee <mbuthiadenis@gmail.com>

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,7 +7,7 @@ import 'react-native-reanimated';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 // Import your global CSS file
-import "../global.css"
+//import "../global.css"
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
